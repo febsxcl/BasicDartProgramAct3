ae9de11d8aa9ed543f9e3e6a0ff7f1ea82b52632
“mark <mark.evang22@gmail.com>
2025-08-03 19:42:41 +0800

    Dart simple calculator program

---
+++ main.dart
@@ -0,0 +1,18 @@
+void main () {
+    int numOne = 6;
+    int numTwo = 22;
+
+    int sum = numOne + numTwo;
+    int diff = numOne - numTwo;
+    int multiply = numOne * numTwo;
+    double div = numOne / numTwo;
+
+    print("The first number is $numOne");
+    print("The second number is $numTwo");
+    print("The total sum is $sum");
+    print("The total diff is $diff");
+    print("The total multiply is $multiply");
+    print("The total div is $div");
+
+
+}
